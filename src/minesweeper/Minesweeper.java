d8e88dfcc4d7fdbf3bfe1bd73eaf742f8e5dd8d9
Umayrkaleem <Umayrkaleem@gmail.com>
2020-07-28 16:21:27 -0500

    adding test cases, so I can code to pass them

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -1,4 +1,13 @@
 package minesweeper;
 
-public class Minesweeper {
+public class Minesweeper {private static int BOUNDS = 10;
+    private static int nMINES = 10;
+    private boolean gameStatus = Boolean.TRUE;
+    private boolean hasWon = Boolean.FALSE;
+
+    private CellState[][]cellStates = new CellState[BOUNDS][BOUNDS];
+    private int[][]cellValues = new int[BOUNDS][BOUNDS];
+
+
+
 }
--- test/minesweeper/MineSweeperTest.java
+++ test/minesweeper/MineSweeperTest.java
@@ -2,9 +2,23 @@ package minesweeper;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MineSweeperTest {
     @Test
     void canary(){
         assert(true);
+    }
+
+    @Test
+    void exposeUnexposedCell() {
+        minesweeper.exposeCell(0, 1);
+        assertEquals(CellState.EXPOSED, minesweeper.getCellState(0, 1));
+    }
+
+    @Test
+    void exposeAnExposedCell() {
+        minesweeper.exposeCell(0, 1);
+        assertEquals(CellState.EXPOSED, minesweeper.getCellState(0, 1));
     }
 }
